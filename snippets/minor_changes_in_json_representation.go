9e4f342ed892c57a101a0ea27e3e6906526d93e7
Joseph Buchma <josephbuchma@gmail.com>
2017-07-19 03:00:22 +0300

    note: Shortcut for minor changes of struct's JSON representation
    twitter_username: josephbuchma
    
    source: http://choly.ca/post/go-json-marshalling

---
+++ snippets/minor_changes_in_json_representation.go
@@ -0,0 +1,10 @@
+func (u *MyUser) MarshalJSON() ([]byte, error) {
+	type Alias MyUser
+	return json.Marshal(&struct {
+		LastSeen int64 `json:"lastSeen"`
+		*Alias
+	}{
+		LastSeen: u.LastSeen.Unix(),
+		Alias:    (*Alias)(u),
+	})
+}
