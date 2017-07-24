7fd5c3e2a3929836cb75797425de9f5464f4efda
Joseph Buchma <josephbuchma@gmail.com>
2017-07-24 09:49:58 +0300

    note: Shuffle slice function (go>=1.8)
    twitter_username: josephbuchma
    
    source: 'https://stackoverflow.com/a/45274033/7582250'

---
+++ snippets/shuffle_slice_func.go
@@ -0,0 +1,9 @@
+func Shuffle(slice interface{}) {
+	rv := reflect.ValueOf(slice)
+	swap := reflect.Swapper(slice)
+	length := rv.Len()
+	for i := length - 1; i > 0; i-- {
+		j := rand.Intn(i + 1)
+		swap(i, j)
+	}
+}
